1d1989f994b8b73c13d8c7ae81816990944f7104
Satoshi Kawasaki <kawasakis0706@gmail.com>
2021-03-30 16:00:44 +0900

    ベンチマーク測定を行うテストコードを実装。

---
+++ src/serverless/ServerlessTsPlain.scala
@@ -0,0 +1,24 @@
+package serverless
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class ServerlessTsPlain extends Simulation {
+
+  val httpConfig = http
+    .baseURL(
+      "https://84s0gzk2jj.execute-api.ap-northeast-1.amazonaws.com/dev"
+    )
+    .acceptHeader(
+      "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    )
+    .doNotTrackHeader("1");
+
+  val scn = scenario("DIライブラリを使わないで実装したAWS Lambdaの性能測定").exec(
+    http("Montecalro Plain").get("/montecarlo").check(status.is(200))
+  );
+
+  setUp(scn.inject(constantUsersPerSec(1) during (1000 seconds)))
+    .protocols(httpConfig);
+}
